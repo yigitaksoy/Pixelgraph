5ec9562c7bb6d45ff43a61d84e31e8a4723f8a09
yigitaksoy <yigitaksoy@gmail.com>
2023-02-05 18:46:34 +0100

    Add download button for the ImageCard component

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,16 +4,39 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
   card: {
     maxWidth: 445,
     marginTop: "2rem",
   },
+  cardContent: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  downloadButton: {
+    alignSelf: "center",
+    marginTop: ".7rem",
+    backgroundColor: "black",
+    color: "#f8f9fa",
+    "&:hover": {
+      backgroundColor: "#f8f9fa",
+      color: "black",
+    },
+  },
 });
 
 function ImageCard({ image }) {
   const classes = useStyles();
+
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = image.webformatURL;
+    link.setAttribute("download", `${image.id}.jpg`);
+    link.click();
+  };
 
   return (
     <div>
@@ -24,7 +47,7 @@ function ImageCard({ image }) {
           height="365"
           image={image.webformatURL}
         />
-        <CardContent>
+        <CardContent className={classes.cardContent}>
           <Typography variant="body2" align="center" component="p">
             <strong>Photo by: </strong>
             {image.user}
@@ -33,6 +56,13 @@ function ImageCard({ image }) {
             <strong># </strong>
             {image.tags}
           </Typography>
+          <Button
+            variant="contained"
+            className={classes.downloadButton}
+            onClick={handleDownload}
+          >
+            Download
+          </Button>
         </CardContent>
       </Card>
     </div>
